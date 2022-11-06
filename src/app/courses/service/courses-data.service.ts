153d6772aa015de36ac28b6a9cff93524819ebee
dwadasi siva teja <sivateja1034@gmail.com>
2022-11-06 18:05:19 +0530

    Feature/courses (#5)
    * added the courses
    
    * added files
    
    * added files
    
    * added changes

--- src/app/courses/service/courses-data.service.ts
+++ src/app/courses/service/courses-data.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
+import { Observable, map } from 'rxjs';
+import { Course } from '../model/course.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CoursesDataService {
+export class CoursesDataService extends DefaultDataService<Course> {
+  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
+    super('Course', http, httpUrlGenerator);
+  }
 
-  constructor() { }
+   override getAll(): Observable<Course[]> {
+    return this.http.get('/api/courses').pipe(map(res => res['payload']));
+  }
 }
