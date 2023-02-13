bddd44d4b2ccde0dc5597310c1a1400ca98c7a01
dwadasi siva teja <sivateja1034@gmail.com>
2023-02-13 17:35:24 +0530

    Feature/completed entity (#13)
    * added resolver
    
    * added the changes entity format
    
    * implemented the entity fetch and auto load method
    
    * implemented the ngrx entity
    
    ---------
    
    Co-authored-by: siva teja <siva.teja@inncrewin.com>

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,3 +1,4 @@
+import { Update } from "@ngrx/entity";
 import { createAction, props } from "@ngrx/store";
 import { Course } from "./model/course";
 
@@ -8,4 +9,10 @@ export const loadAllCourses = createAction(
 export const allCoursesLoaded = createAction(
   "[Load Courses Effect] All Courses are Loaded",
   props<{ courses: Course[] }>()
+);
+
+// action for the updating of the course
+export const courseUpdate = createAction(
+  "[Edit course Dialog] course updated",
+  props<{ update: Update<Course> }>()
 );
